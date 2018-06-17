97411238171ffb8761ed1e2fa1fbdcd1251c613f
Daniel Costa <ccdanielcosta@gmail.com>
2018-06-17 04:51:39 +0100

    Received message

--- app/src/main/java/com/example/carloscosta/camandroid/ReceiveMessage.java
+++ app/src/main/java/com/example/carloscosta/camandroid/ReceiveMessage.java
@@ -1,5 +1,9 @@
 package com.example.carloscosta.camandroid;
 
+import com.googlecode.protobuf.format.JsonFormat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -17,19 +21,30 @@ public class ReceiveMessage implements Runnable {
         System.out.println("Waiting for data");
         byte[] recData;
         int len;
-        while (true) {
-            try {
-                System.out.println("RECEBIDO");
-                datagramPacket = new DatagramPacket(data, data.length);
-                datagramSocket.receive(datagramPacket);
-                len = datagramPacket.getLength();
-                recData = new byte[len];
-                System.arraycopy(datagramPacket.getData(), 0, recData, 0, datagramPacket.getLength());
-                Message.MessageP2A messageP2A = Message.MessageP2A.parseFrom(recData);
-                System.out.println("Received P2A Message: " + messageP2A.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        while (true) try {
+            System.out.println("RECEBIDO");
+            datagramPacket = new DatagramPacket(data, data.length);
+            datagramSocket.receive(datagramPacket);
+            len = datagramPacket.getLength();
+            recData = new byte[len];
+            System.arraycopy(datagramPacket.getData(), 0, recData, 0, datagramPacket.getLength());
+
+            Message.MessageP2A messageP2A = Message.MessageP2A.parseFrom(recData);
+           // JsonFormat.printToString(messageP2A);
+
+          //  System.out.println("UU: " + s);
+
+
+            System.out.println("Received P2A Message: " + messageP2A.toString());
+            //  System.out.println("3: " +  JsonFormat.printToString(protoMessage));
+
+
+            // String uniURL = uniObject.getJsonString("url");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+
+
 }
