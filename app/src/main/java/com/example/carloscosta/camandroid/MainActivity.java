8dbe616e62583b13e1867355cc397f1c6032741b
Unknown <pg36092@alunos.uminho.pt>
2018-06-30 12:10:59 +0100

    fgfdgg

--- app/src/main/java/com/example/carloscosta/camandroid/MainActivity.java
+++ app/src/main/java/com/example/carloscosta/camandroid/MainActivity.java
@@ -56,6 +56,8 @@ public class MainActivity extends AppCompatActivity  {
         progressBar = (ProgressBar) findViewById(R.id.progressBar_cyclic);
         setToastandAudio();
 
+        
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
        // mapFragment.getMapAsync(this);
