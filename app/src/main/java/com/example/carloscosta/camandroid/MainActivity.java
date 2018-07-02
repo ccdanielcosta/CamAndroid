649b31313a0242eb1adcabf177f90aefb4624559
Unknown <pg36092@alunos.uminho.pt>
2018-07-02 19:30:50 +0100

    Settings menu
    add setting resolve conflicts

--- app/src/main/java/com/example/carloscosta/camandroid/MainActivity.java
+++ app/src/main/java/com/example/carloscosta/camandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.carloscosta.camandroid;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
@@ -12,6 +13,8 @@ import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -77,6 +80,32 @@ public class MainActivity extends AppCompatActivity  {
         new Thread(p).start();
 
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            new AlertDialog.Builder(this)
+                    .setTitle("uiui")
+                    .setMessage("ola")
+                    .show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
